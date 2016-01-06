bee4177d8f0d9bce943e7138233850fbaf180b46
Art Navsegda <artnavsegda@gmail.com>
2016-01-06 10:07:53 +0000

    sensor fix

--- src/asf.h
+++ src/asf.h
@@ -55,9 +55,25 @@
 // From module: ADC - XMEGA A/AU Implementation
 #include <adc.h>
 
+// From module: Atmel QTouch Library - Common files
+#include <touch_api.h>
+#include <touch_qt_config.h>
+
 // From module: CPU specific features
 #include <ccp.h>
 #include <xmega_reset_cause.h>
+
+// From module: Delay routines
+#include <delay.h>
+
+// From module: GFX Monochrome - Menu System
+#include <gfx_mono_menu.h>
+
+// From module: GFX Monochrome - Monochrome Graphic Library
+#include <gfx_mono.h>
+
+// From module: GFX Monochrome - System Font
+#include <sysfont.h>
 
 // From module: GPIO - General purpose Input/Output
 #include <gpio.h>
@@ -80,6 +96,19 @@
 // From module: Part identification macros
 #include <parts.h>
 
+// From module: SPI - Serial Peripheral Interface
+#include <spi.h>
+
+// From module: SPI - XMEGA implementation
+#include <usart_spi.h>
+#include <xmega_usart_spi/usart_spi.h>
+
+// From module: ST7565R LCD controller
+#include <st7565r.h>
+
+// From module: Sensors - Common Sensor Service
+#include <sensor.h>
+
 // From module: Sleep Controller driver
 #include <sleep.h>
 
@@ -87,8 +116,27 @@
 #include <sleepmgr.h>
 #include <xmega/sleepmgr.h>
 
+// From module: Standard serial I/O (stdio) - XMEGA implementation
+#include <stdio_serial.h>
+
 // From module: System Clock Control - XMEGA A1U/A3U/A3BU/A4U/B/C implementation
 #include <sysclk.h>
+
+// From module: TWI - Two-Wire Interface - XMEGA implementation
+#include <twi_master.h>
+#include <twi_slave.h>
+#include <xmega_twi/twi_master.h>
+#include <xmega_twi/twi_slave.h>
+
+// From module: TWI - Two-wire Master and Slave Interface
+#include <twim.h>
+#include <twis.h>
+
+// From module: USART - Serial interface - XMEGA implementation
+#include <serial.h>
+
+// From module: USART - Universal Synchronous/Asynchronous Receiver/Transmitter
+#include <usart.h>
 
 // From module: XMEGA compiler driver
 #include <compiler.h>
